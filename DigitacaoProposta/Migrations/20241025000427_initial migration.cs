d72f66a4f412fe0cee2a92aa08e7d211df1dd257
Luiz Gustavo Alberti <gustavo.dev.poa@outlook.com>
2024-10-24 21:08:07 -0300

    add migration

---
+++ DigitacaoProposta/Migrations/20241025000427_initial migration.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DigitacaoProposta.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RendimentoMensal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Cidade = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Uf = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    StatusCpf = table.Column<string>(type: "varchar(20)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Conveniadas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    AceitaRefinanciamento = table.Column<bool>(type: "bit", nullable: false),
+                    Uf = table.Column<string>(type: "nchar(2)", fixedLength: true, maxLength: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conveniadas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estados",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Uf = table.Column<string>(type: "nchar(2)", fixedLength: true, maxLength: 2, nullable: false),
+                    RestricaoDeValor = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estados", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Agentes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    varchar20 = table.Column<string>(name: "varchar(20)", type: "nvarchar(max)", nullable: false),
+                    ConveniadaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agentes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Agentes_Conveniadas_ConveniadaId",
+                        column: x => x.ConveniadaId,
+                        principalTable: "Conveniadas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Propostas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CpfCliente = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    ValorEmprestimo = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    NumeroParcelas = table.Column<int>(type: "int", nullable: false),
+                    ValorParcela = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DataPrimeiraParcela = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AgenteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ConveniadaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "varchar(20)", nullable: false),
+                    TipoOperacao = table.Column<string>(type: "varchar(20)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Propostas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Propostas_Agentes_AgenteId",
+                        column: x => x.AgenteId,
+                        principalTable: "Agentes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Propostas_Conveniadas_ConveniadaId",
+                        column: x => x.ConveniadaId,
+                        principalTable: "Conveniadas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agentes_ConveniadaId",
+                table: "Agentes",
+                column: "ConveniadaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Propostas_AgenteId",
+                table: "Propostas",
+                column: "AgenteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Propostas_ConveniadaId",
+                table: "Propostas",
+                column: "ConveniadaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Estados");
+
+            migrationBuilder.DropTable(
+                name: "Propostas");
+
+            migrationBuilder.DropTable(
+                name: "Agentes");
+
+            migrationBuilder.DropTable(
+                name: "Conveniadas");
+        }
+    }
+}
